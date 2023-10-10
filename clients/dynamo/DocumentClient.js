fe09ab29733d25a5986328a57679e12ac282d253
tom <tomy.m@diagnal.com>
2023-10-10 11:18:14 +0530

    Async DB Queries Updated

--- clients/dynamo/DocumentClient.js
+++ clients/dynamo/DocumentClient.js
@@ -113,6 +113,129 @@ class DocumentClient {
       }
     })
   }
+
+  getAsync (params, callback) {
+    return new Promise((resolve, reject) => {
+      const getCommand = new GetCommand(params)
+      documentClient.send(getCommand, (err, response) => {
+        if (err || !response) {
+          return reject(err)
+        }
+        if (response.Item) {
+          return resolve(response)
+        } else if (!response.Item) {
+          return resolve({})
+        }
+      })
+    }).then(result => {
+      if (callback && typeof callback === 'function') {
+        callback(null, result)
+      }
+      return result
+    }).catch(error => {
+      if (callback && typeof callback === 'function') {
+        callback(error, null)
+      }
+      return Promise.reject(error)
+    })
+  }
+
+  queryAsync (params, callback) {
+    return new Promise((resolve, reject) => {
+      const getCommand = new QueryCommand(params)
+      documentClient.send(getCommand, (err, response) => {
+        if (err || !response) {
+          return resolve(err)
+        }
+        return resolve(response)
+      })
+    }).then(result => {
+      if (callback && typeof callback === 'function') {
+        callback(null, result)
+      }
+      return result
+    }).catch(error => {
+      if (callback && typeof callback === 'function') {
+        callback(error, null)
+      }
+      return Promise.reject(error)
+    })
+  }
+
+  putAsync (params, callback) {
+    return new Promise((resolve, reject) => {
+      let item = params.Item
+      try {
+        item = Object.fromEntries(Object.entries(item).filter(([key, value]) => value !== undefined))
+      } catch (error) {
+
+      }
+      params.Item = item
+      const putCommand = new PutCommand(params)
+      documentClient.send(putCommand, (err, response) => {
+        if (err || !response) {
+          return resolve(err)
+        }
+        return resolve(response)
+      })
+    }).then(result => {
+      if (callback && typeof callback === 'function') {
+        callback(null, result)
+      }
+      return result
+    }).catch(error => {
+      if (callback && typeof callback === 'function') {
+        callback(error, null)
+      }
+      return Promise.reject(error)
+    })
+  }
+
+  deleteAsync (params, callback) {
+    return new Promise((resolve, reject) => {
+      const docClient = DynamoDBDocument.from(new DynamoDB())
+      docClient.delete(params, (err, response) => {
+        if (err) {
+          return resolve(err)
+        } else {
+          return resolve(response)
+        }
+      })
+    }).then(result => {
+      if (callback && typeof callback === 'function') {
+        callback(null, result)
+      }
+      return result
+    }).catch(error => {
+      if (callback && typeof callback === 'function') {
+        callback(error, null)
+      }
+      return Promise.reject(error)
+    })
+  }
+
+  updateAsync (params, callback) {
+    return new Promise((resolve, reject) => {
+      const docClient = DynamoDBDocument.from(new DynamoDB())
+      docClient.update(params, (err, response) => {
+        if (err) {
+          return resolve(err)
+        } else {
+          return resolve(response)
+        }
+      })
+    }).then(result => {
+      if (callback && typeof callback === 'function') {
+        callback(null, result)
+      }
+      return result
+    }).catch(error => {
+      if (callback && typeof callback === 'function') {
+        callback(error, null)
+      }
+      return Promise.reject(error)
+    })
+  }
 }
 
 module.exports = DocumentClient
